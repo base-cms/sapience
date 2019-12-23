e966e91dfbd0555966e0f4d67e4a1cceda469e9e
Jacob Bare <jacob@limit0.io>
2019-12-23 09:10:30 -0600

    Create initial webpack config

---
+++ packages/js/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: './dist',
+    filename: 'sapience.js',
+  },
+};
